0731610cff549aa8ab27d80f4d55d83623222d74
jesse <jessefortuin850@gmail.com>
2023-03-29 15:37:52 +0200

    Feat: Method implemented

--- CreditCardMask.BL/MaskingDetails.cs
+++ CreditCardMask.BL/MaskingDetails.cs
@@ -1,13 +1,23 @@
-﻿namespace CreditCardMask.BL
+﻿using System.Text;
+
+namespace CreditCardMask.BL
 {
     public class MaskingDetails : IMaskingDetails
     {
         public string Maskify(string cc)
         {
+            if (cc.Length <= 4)
+            {
+                return cc;
+            }
 
+            StringBuilder builder = new StringBuilder(cc);
 
+            builder.Remove(0, cc.Length - 4);
 
-            return "";
+            builder.Insert(0, "#", cc.Length - 4);
+
+            return builder.ToString();
         }
     }
 }
